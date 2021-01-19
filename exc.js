13dc52dc825164681bc8800ccc35632ef1f46405
Aldair Avalos <aldair.avalos.2000@gmail.com>
2021-01-18 18:42:37 -0600

    validations for arrays done

--- exc.js
+++ exc.js
@@ -124,10 +124,14 @@
 
 // 21) Programa una función que dado un array numérico devuelve otro array con los números elevados al cuadrado, pe. mi_funcion([1, 4, 5]) devolverá [1, 16, 25].
 
-// const sqNumArray = array => {
-//     if (array === undefined) return console.warn ('No metiste un array');
-//     if( !(array instanceof Array)) return console.error('Ingresa un array con numeros');
-//     if (array === null) return console.warn ('No metiste un nombre');
+const sqNumArray = array => {
+    if (array === undefined) return console.warn ('No metiste un array');
+    if( !(array instanceof Array)) return console.error('Ingresa un array con numeros');
+    if (array.length === 0) return console.warn ('El array está vacío');
+    for (let num of array){
+        if (typeof num !== 'number') {
+            return console.error('Los valores del array no son numeros')};
+    };
 
 //     return array.map(number => {
 //         return number ** 2;
@@ -140,6 +144,14 @@
 
 // 22) Programa una función que dado un array devuelva el número mas alto y el más bajo de dicho array, pe. miFuncion([1, 4, 5, 99, -60]) devolverá [99, -60].
 // const minAndMax = array => {
+    // if (array === undefined) return console.warn ('No metiste un array');
+    // if( !(array instanceof Array)) return console.error('Ingresa un array con numeros');
+    // if (array.length === 0) return console.warn ('El array está vacío');
+    // for (let num of array){
+    //     if (typeof num !== 'number') {
+    //         return console.error('Los valores del array no son numeros')};
+    // };
+
 //     let min = Math.min(...array);
 //     console.log(min);
 //     let max = Math.max(...array);
@@ -161,6 +173,14 @@
 // 23) Programa una función que dado un array de números devuelva un objeto con 2 arreglos en el primero almacena los números pares y en el segundo los impares, pe. miFuncion([1,2,3,4,5,6,7,8,9,0]) devolverá {pares: [2,4,6,8,0], impares: [1,3,5,7,9]}.
 
 // const evenAndOdds = numbersArray => {
+    // if (array === undefined) return console.warn ('No metiste un array');
+    // if( !(array instanceof Array)) return console.error('Ingresa un array con numeros');
+    // if (array.length === 0) return console.warn ('El array está vacío');
+    // for (let num of array){
+    //     if (typeof num !== 'number') {
+    //         return console.error('Los valores del array no son numeros')};
+    // };
+    
 //     let even = [];
 //     let odd = [];
 //     numbersArray.map(number => {
