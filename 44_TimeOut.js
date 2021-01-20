c4ae1c20cc89f6e9ab88aaf77b4d7ffa1329db17
Aldair Avalos <aldair.avalos.2000@gmail.com>
2021-01-20 11:33:54 -0600

    setTimeou y setInterval y clear

---
+++ 44_TimeOut.js
@@ -0,0 +1,21 @@
+console.log('Start');
+//Timeout
+setTimeout(() => {
+    console.log(`Ejecutando un setTimeout esto se ejecuta una sola vez`);
+}, 3000);
+//Interval
+let interval = setInterval(() => {
+    console.log('Ejecutando setInterval, se ejecuta indefinidamente cada que pase el tiempo que se indique');
+}, 1000);
+
+//In order to stop the interval we could use the clearTimeOut() method, the setInterval must be located in a variable.
+let clock = setInterval(() => {
+    console.log(new Date().toTimeString());
+}, 1000);
+
+setTimeout(() => {
+    clearInterval(clock);    
+}, 5000);
+setTimeout(() => {
+    clearInterval(interval);    
+}, 7000);
