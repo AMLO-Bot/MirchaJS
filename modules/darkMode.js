a4da8fc0b8e516fe199d21afa087b30e6ae74b65
Aldair Avalos <aldair.avalos.2000@gmail.com>
2021-03-23 14:14:13 -0600

    stored darkmode in localStorage

--- modules/darkMode.js
+++ modules/darkMode.js
@@ -1,69 +1,44 @@
-//Esta implementacion se puede mejorar, en vez de traer todos los elementos que necesites cambiar mejor utilizamos un atributo custom en html para indicar que elementos van a cambiar de color, ese elementos los fetcheamos para luego añadirles la clase con el css del dark mode
 export function darkMode(darkBtnSelector, classDark, classDarkAlt) {
-  const localStorage = window.localStorage;
-
-  const d = document,
+  const localStorage = window.localStorage,
+  d = document,
   $darkBtn = d.querySelector(darkBtnSelector),
+  //Aplicamos un data-dark como atributo a los elementos que van a ser afectados por el darkMode
   $selectorsDark = d.querySelectorAll("[data-dark]"), 
   $selectorsDarkAlt = d.querySelectorAll("[data-darkAlt]"); 
 
-  // Atributo para indetificar elementos que van a ser afectados por dark mode y paleta secundaria de darkmode
   let moon = "🌙", 
   sun = "🌞";
   //Dont know why, but $darkBtn.textContent start with several blank spaces so we just trim()'ed them.
-  $darkBtn.textContent = $darkBtn.textContent.trim()
+  $darkBtn.textContent = $darkBtn.textContent.trim();
 
-  //Check if darkMode was enabled to load the correct layout
-  console.log(localStorage.getItem("darkMode"));
-  if (localStorage.getItem("darkMode")) {
-    removeDarkMode();
-  }else{
-    console.log(window.localStorage);
-    console.log("ksdjhf")
-    addDarkMode();
-  };
-  
-  //Adds the classDark to all elements with the data-dark attrib in the html
-  function addDarkMode(){
-    $selectorsDark.forEach(el => {
-      el.classList.add(classDark);
-    });
-    
-    $selectorsDarkAlt.forEach(el => {
-      el.classList.add(classDarkAlt);
-    });
-
+  //Aplica la classDark, que tiene los estilos del darkMode, a todos los elementos fetcheados
+  function addDarkMode() {
+    $selectorsDark.forEach(el => el.classList.add(classDark));
+    $selectorsDarkAlt.forEach(el => el.classList.add(classDarkAlt));
     $darkBtn.textContent = sun;
   };
   function removeDarkMode() {
-    
-    $selectorsDark.forEach(el => {
-      el.classList.remove(classDark);
-    });
-    
-    $selectorsDarkAlt.forEach(el => {
-      el.classList.remove(classDarkAlt);
-    });
-
+    $selectorsDark.forEach(el => el.classList.remove(classDark));
+    $selectorsDarkAlt.forEach(el => el.classList.remove(classDarkAlt));
     $darkBtn.textContent = moon;
   };
 
+  //Check if darkMode had been applied before
+  if(localStorage.getItem("isDarkMode") === "true"){
+    addDarkMode();
+  }
+  
   //Toggle darkMode btn 
   d.addEventListener("click", ev => {
-
-    if (ev.target.matches(darkBtnSelector) || ev.target.matches(`${darkBtnSelector} *`)) {
-      
+    
+    if (ev.target.matches(darkBtnSelector)) {
       if($darkBtn.textContent === moon){
-        addDarkMode()
-        localStorage.setItem("darkMode",true);
-        console.log(localStorage.getItem("darkMode"));
+        addDarkMode();
+        localStorage.setItem("isDarkMode",true);
       }else{
         removeDarkMode();
-        localStorage.setItem("darkMode",false);
-        console.log(localStorage.getItem("darkMode"));
+        localStorage.setItem("isDarkMode",false);
       };
-
     };
-
   });
 };
