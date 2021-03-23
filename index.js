e2a1da650739fbf4773f89f967b7d3ec5a3a76fc
Aldair Avalos <aldair.avalos.2000@gmail.com>
2021-03-23 16:33:52 -0600

    WIP responsive multimedia elements

--- index.js
+++ index.js
@@ -4,8 +4,16 @@ import {ball, notify} from "./modules/keyboard.js";
 import {countdown} from "./modules/countdown.js"
 import { toTop } from "./modules/toTop.js";
 import { darkMode } from "./modules/darkMode.js";
+import { responsiveJS } from "./modules/responsiveJS.js";
 
-
+const links = {
+  cumbion: "https://www.youtube.com/watch?v=tWwWoDFoubw&list=PLQlR3Vw0ooxcNUhsgaid-gYu_YKNQRXHa&index=15",
+  mapa: "https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=es&amp;q=Les%20Rambles,%201%20Barcelona,%20Spain+(Mi%20nombre%20de%20egocios)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed",
+};
+const iframes ={
+  videoCumbion:`<iframe width="560" height="315" src=\"https://www.youtube.com/embed/bdYks30p1FE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>`,
+  mapa: `<iframe width="560" height="315" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src=\"https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=es&amp;q=Les%20Rambles,%201%20Barcelona,%20Spain+(Mi%20nombre%20de%20egocios)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed\"></iframe>`,
+}
 const d = document;
 //Cuando se parsee el dom entonces asignamos los eventos pertinentes al docuemnto
 d.addEventListener("DOMContentLoaded", ev => {
@@ -13,13 +21,15 @@ d.addEventListener("DOMContentLoaded", ev => {
   digitalClock(".clock",".btns--start",".btns--stop");
   alarm("./assets/alarm.mp3",".btns--alarm-on",".btns--alarm-off");
   countdown(".countdown",'December 21, 1995 03:24:00',"FELIZ CUMPLEAÑOS !!!! 🎈✨🎉🎊");
-  toTop(".to-top-btn","to-top-btn--transition")
+  toTop(".to-top-btn","to-top-btn--transition");
+  responsiveJS(links,iframes);
 });
 // cumpleanos = new Date(1995,11,17,3,24,0);
 d.addEventListener("keydown", ev =>{
   ball(ev, ".ball", ".playground");
   notify(ev);
 })
+
 //
 darkMode(".dark-mode-btn","dark-mode","dark-mode-alt");
 
---
+++ modules/responsiveJS.js
@@ -0,0 +1,39 @@
+export function responsiveJS(links, iframes,mobileWidth = 400){
+  //constants
+  const d = document,
+  $seccion4 = d.querySelector("#seccion4");
+  
+  //Multimedia ELements
+  const mobileMultimedia = () => {
+    console.log("TABLET");
+    let iframesDiv = d.createElement("div");
+    for (const iframeHTML in iframes) {
+      iframesDiv.innerHTML += iframes[iframeHTML]
+    };
+    $seccion4.append(iframesDiv);
+  };
+
+  const tabletMultimedia = () => {
+    console.log("MOBILE");
+    for (const domain in links) {
+      let aEl = d.createElement("a");
+      aEl.setAttribute("href", links[domain]);
+      aEl.setAttribute("target","_blank")
+      aEl.textContent = `Ver ${domain} en página de origen`;
+      // aEl.style.display = "block"
+      // aEl.style.color = "red"
+      console.log(aEl)
+      $seccion4.appendChild(aEl);
+    }; 
+  };
+
+  //Pick which element will be loaded depending of display size => computing power of device
+  if (window.matchMedia(`(min-width: ${mobileWidth}px)`).matches) {
+    mobileMultimedia();
+  } else { 
+    tabletMultimedia();
+  };
+
+  
+
+};
