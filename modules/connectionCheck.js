46b33960f2803815a2cf735cfb46fa3e52075f92
Aldair Avalos <aldair.avalos.2000@gmail.com>
2021-03-25 13:33:39 -0600

    DONE banner offline online

--- modules/connectionCheck.js
+++ modules/connectionCheck.js
@@ -1,4 +1,23 @@
-const d = document;
-export function connectionCheck(bannerClass, classOnline, classOffline){
-  console.log(bannerClass)
+const d = document,
+w = window;
+export function connectionCheck(classBanner, classOnline, classOffline){
+  const $banner = d.querySelector(classBanner);
+  w.addEventListener("offline", ev => {
+    $banner.textContent = "SE PERDIÓ LA CONEXIÓN 😥";
+    $banner.classList.add(classOffline);
+
+  });
+  w.addEventListener("online", ev => {
+    $banner.textContent = "REGRESÓ LA CONEXIÓN 😂";
+    $banner.classList.add(classOnline);
+
+    setTimeout(() => {
+      $banner.classList.remove(classOffline)
+      $banner.classList.remove(classOnline);
+    }, 2000);
+  });
+
+
+
 };
+
