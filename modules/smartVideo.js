fc2a37180bc2ac16f4272b91d6fdad252850e4d8
Aldair Avalos <aldair.avalos.2000@gmail.com>
2021-04-09 21:29:50 -0500

    DONE smartVideo module

--- modules/smartVideo.js
+++ modules/smartVideo.js
@@ -1,11 +1,29 @@
 const d = document;
-export function smartVideo(videoId) {
-  const $smartVideo = d.getElementById(videoId);
-  const options = {treshold: 0.7};
-  const observer = new IntersectionObserver(videoHandler, options);
-  observer.observe($smartVideo);
-  
-  function videoHandler(entry, observer){
-    console.log(entry.target)
+
+export function smartVideo(videoClass) {
+  //Fetch all smartVideos
+  const $videos = d.getElementsByClassName(videoClass);
+  //Initialize intersection observer and observe every video in $videos
+  const options = {threshold: 0.7};
+  const observer = new IntersectionObserver(videosHandler, options);
+  for (const video of $videos) {
+    observer.observe(video);
   }
+
+  
+  function videosHandler(entries, observer){
+    entries.forEach(entry => {
+      if(entry.isIntersecting) entry.target.play();
+      if(!entry.isIntersecting) entry.target.pause();
+    });
+  }
+
+  //Add pause/play to videos on tab changing
+  //Convert HTMLCollection to array in order to use map
+  const videos = [...$videos];
+  function handleVisibilityChange() {
+    if (d.hidden) videos.map(video => video.pause());
+    if (!d.hidden) videos.map(video => video.play());
+  }
+  d.addEventListener("visibilitychange", handleVisibilityChange)
 }
