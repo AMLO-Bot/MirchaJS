e529dec0af6d02fa24c1cd696f76d424f197915b
Aldair Avalos <aldair.avalos.2000@gmail.com>
2021-04-13 11:29:29 -0500

    XHR for json placeholder

--- xhr.js
+++ xhr.js
@@ -1,8 +1,29 @@
-const d = document;
-const xhr = new XMLHttpRequest();
+(
+  () => {
+  const d = document;
+  const xhr = new XMLHttpRequest();
+  const $list = d.getElementById("ajax-list");
+  const $fragment = d.createDocumentFragment();
 
-d.addEventListener("readystatechange", (ev) => {
-  console.log(ev)
-})
-xhr.open("GET", "https://jsonplaceholder.typicode.com/posts");
-xhr.send();
+  xhr.addEventListener("readystatechange", (ev) => {
+    if (xhr.readyState !== 4) return;
+    
+    if (xhr.status > 199 && xhr.status < 300) {
+      console.log("TODO OK")
+      const json = JSON.parse(xhr.responseText);
+      json.forEach(el => {
+        let $li = d.createElement("li");
+        $li.textContent = `User: ${el.userId}  -- Title: ${el.title}`;
+        $fragment.appendChild($li);
+      });
+      $list.appendChild($fragment);
+    }else{
+      console.log("ERROR")
+      let msg = xhr.responseText || "Unexpected Error Ocurred"
+      console.warn(`${xhr.status}: ${msg}`)
+    };
+  });
+  
+  xhr.open("GET", "https://jsonplaceholder.typicode.com/posts");
+  xhr.send()
+})();
