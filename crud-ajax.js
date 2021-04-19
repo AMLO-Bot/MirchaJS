9fb637f526732528ac157f7281767f76ff2960b8
Aldair Avalos <aldair.avalos.2000@gmail.com>
2021-04-19 16:59:17 -0500

    add reading lesson in bd.json

--- crud-ajax.js
+++ crud-ajax.js
@@ -5,61 +5,68 @@ $form = d.querySelector(".crud-form"),
 $title = d.querySelector(".crud-title"),
 $fragment = d.createDocumentFragment();
 
-//Request all the data to fill the table
-const printData = function() {
-  const xhrHandler = function() {
+const ajax = (options) => {
+  let {url, method = "GET", successCb, errorCb, data = "null"} = options;
+  const xhr = new XMLHttpRequest();
+
+  xhr.open(method, url);
+  xhr.setRequestHeader("Content-Type", "application/json; charset=utf-8")
+  xhr.addEventListener("readystatechange", ev => {
     if (xhr.readyState !== 4) return;
     if (xhr.status > 199 && xhr.status < 300){
       let json = JSON.parse(xhr.response);
-      console.log(json);
-      
-      json.forEach(knigth => { 
-        $template.querySelector(".name").textContent = knigth.name;
-        $template.querySelector(".constellation").textContent = knigth.constellation;
-        $template.querySelector(".metal").textContent = knigth.metal;
-        const $newKnigth = d.importNode($template, true);
-        $table.querySelector("tbody").appendChild($newKnigth)
-      });
-    };
-  }
-  const xhr = new XMLHttpRequest();
-  xhr.open("GET", "http://127.0.0.1:5000/knigths");
-  xhr.addEventListener("readystatechange", xhrHandler, true);
-  xhr.send();
+      successCb(json);
+    }else{}
+      let msg = xhr.statusText || "An unexpected error ocurred"
+      errorCb(`An error ocurred /n ${xhr.status}: ${msg} `)
+  })
+  xhr.send(JSON.stringify(data));
+}
+
+function errorCb(error){
+  return new Error(error);
+}
+
+function printKnigths (json) {  
+  console.log(json);
+  json.forEach(knigth => { 
+    $template.querySelector(".name").textContent = knigth.name;
+    $template.querySelector(".constellation").textContent = knigth.constellation;
+    $template.querySelector(".metal").textContent = knigth.metal;
+    const $newKnigth = d.importNode($template, true);
+    $table.querySelector("tbody").appendChild($newKnigth)
+  });
 };
-// Printing all data in db.json at startup
-printData();
 
+//Get all information to display in the tables
+ajax({
+  url:"http://127.0.0.1:8000/knigths",
+  method: "GET",
+  successCb: printKnigths,
+  errorCb: errorCb,
+  data: null
+});
 
-// POST Method
-// Delegating click event to form buttton to perform a POST method
-// $form.addEventListener("submit", ev => {
-//   ev.preventDefault();
+//Post a new Knigth when submitted the form
+$form.addEventListener("submit", ev => {
+  ev.preventDefault();
   
-//   const name = $form.querySelector(`input[name="name"]`).value,
-//   constellation = $form.querySelector(`input[name="constellation"]`).value,
-//   metal = $form.querySelector(`input[name="metal"]`).value;
-  
-//   const newKnigth = new FormData();
-//   newKnigth.append("name", name);
-//   newKnigth.append("constellation", constellation);
-//   newKnigth.append("metal", metal)
-  
-//   const xhr = new XMLHttpRequest();
-//   xhr.open("POST", "http://127.0.0.1:5000/knigths");
-//   xhr.addEventListener("readystatechange", () => {
-//     if (xhr.readyState !== 4) return;
-//     if (xhr.status === 201){
-//       printData();
-//     }else{
-//       return console.error("An error Ocurred, Couldn't fulfilled request")
-//     }
-//   })
-//   xhr.send(newKnigth);
-  
-//   console.log("SUBMITTED")
-// });
-
-
-
-
+  ajax({
+    url:"http://127.0.0.1:8000/knigths",
+    method: "POST",
+    successCb: null,
+    errorCb: errorCb,
+    data: {
+      "name": $form.querySelector(`input[name="name"]`).value,
+      "constellation": $form.querySelector(`input[name="constellation"]`).value,
+      "metal": $form.querySelector(`input[name="metal"]`).value
+    }
+  });
+  ajax({
+    url:"http://127.0.0.1:8000/knigths",
+    method: "GET",
+    successCb: printKnigths,
+    errorCb: errorCb,
+    data: null
+  });
+});
